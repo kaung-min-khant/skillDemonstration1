8f53e7dd0af01ea9597505e58a467f7735e65873
Justin Khant <1kaungminkhant1@gmail.com>
2022-05-01 21:34:06 -0700

    initial commit

---
+++ SkillDemonTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonTest {
+    @Test 
+    public void multiply (){
+        assertEquals(2, SkillDemonstration1.multiply(2,2));
+    }
+}
