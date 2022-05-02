d6081542b044af1863f6e143e2044a123e309954
Justin Khant <1kaungminkhant1@gmail.com>
2022-05-01 21:36:24 -0700

    error fixed

--- SkillDemonTest.java
+++ SkillDemonTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemonTest {
     @Test 
     public void multiply (){
-        assertEquals(2, SkillDemonstration1.multiply(2,2));
+        assertEquals(4, SkillDemonstration1.multiply(2,2));
     }
 }
